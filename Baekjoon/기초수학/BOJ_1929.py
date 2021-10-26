9afa1e986b48085d104ec478d28ebf38ceae0840
kakjzi <anmern3@naver.com>
2021-10-26 22:27:47 +0900

    소수

--- Baekjoon/기초수학/BOJ_1929.py
+++ Baekjoon/기초수학/BOJ_1929.py
@@ -1,10 +1,16 @@
-M,N=map(int,input().split()) 
-prime=[True]*(N+1)
-prime[1]=False 
-for i in range(2,int(N**0.5)+1): 
-    if prime[i]==True: 
-        for j in range(i+i,N+1,i): 
-            prime[j]=False 
-for i in range(N+1): 
-    if prime[i]==True and M<=i: 
-        print(i)
+import math 
+
+def isPrime(num): 
+    if num == 1: 
+        return False 
+    n = int(math.sqrt(num))  #제곱근 
+
+    for k in range(2, n+1): # 소수는 1과 자기자신 따라서 나누어 떨어지면안됨.
+        if num % k == 0:
+            return False 
+    return True 
+
+    
+m, n = map(int, input().split()) 
+for k in range(m, n+1): # 범위는 m <= k <= n 이므로 , 하나씩 함수에 넣어서 true/false 확인하면 될듯.
+    if isPrime(k) : print(k)
